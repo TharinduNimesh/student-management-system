3555a3d797d8c448ef456c692d016f8351fbcac5
Tharindu Nimesh <127307898+TharinduNimesh@users.noreply.github.com>
2023-07-02 15:03:24 +0530

    Initial commit

---
+++ src/model/Mysql.java
@@ -0,0 +1,50 @@
+package model;
+
+import java.sql.*;
+
+public class Mysql {
+
+    private static Connection connection;
+
+    public static void connect(String host, String username, String password, String database) throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://" + host + ":3306/" + database + "", username, password);
+    }
+
+    public static boolean checkConnection() {
+        if (connection.equals("")) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void closeConnection() throws Exception {
+        try {
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected static void insert(String query) throws Exception {
+        run(query);
+    }
+
+    protected static void update(String query) throws Exception {
+        run(query);
+    }
+
+    protected static void delete(String query) throws Exception {
+        run(query);
+    }
+
+    public static ResultSet search(String query) throws Exception {
+        Statement statement = connection.createStatement();
+        return statement.executeQuery(query);
+    }
+
+    private static void run(String query) throws Exception {
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(query);
+    }
+}
